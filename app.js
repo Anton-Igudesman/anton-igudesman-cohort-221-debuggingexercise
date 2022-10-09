69f97394d5495dfa63325328d447be7465cc7214
Anton-Igudesman <antonguy@gmail.com>
2022-10-08 17:56:36 -0700

    "adding second button functionality"

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-function toggleEvenColor() {
-    console.log("clicked")
+function toggleEvenValues() {
     let spanElements = document.querySelectorAll('span');
     spanElements.forEach(item => {
         if (item.textContent % 2 === 0) {
             item.style.backgroundColor = item.style.backgroundColor !== 'green' ? 'green' : 'transparent';
         }
     })
+    }
+
+    function toggleEvenColors() {
+        let spanElements = document.querySelectorAll('span:nth-child(2n)');
+        spanElements.forEach(item => {
+            item.style.backgroundColor = item.style.backgroundColor !== 'pink' ? 'pink' : 'transparent';
+        })
     }
