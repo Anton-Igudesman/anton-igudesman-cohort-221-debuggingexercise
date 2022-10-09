2d385ad524b884bee4f7e3834d4eae8343bca463
Anton-Igudesman <antonguy@gmail.com>
2022-10-08 18:09:03 -0700

    resolving logic issues between 2 buttons

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ function toggleEvenValues() {
     let spanElements = document.querySelectorAll('span');
     spanElements.forEach(item => {
         if (item.textContent % 2 === 0) {
-            item.style.backgroundColor = item.style.backgroundColor !== 'green' ? 'green' : 'transparent';
+            item.style.backgroundColor = item.style.backgroundColor === 'green' && 'pink' ? 'transparent' : 'green';
         }
     })
     }
@@ -10,6 +10,6 @@ function toggleEvenValues() {
     function toggleEvenColors() {
         let spanElements = document.querySelectorAll('span:nth-child(2n)');
         spanElements.forEach(item => {
-            item.style.backgroundColor = item.style.backgroundColor !== 'pink' ? 'pink' : 'transparent';
+            item.style.backgroundColor = item.style.backgroundColor === 'pink' && 'green' ? 'transparent' : 'pink';
         })
     }
